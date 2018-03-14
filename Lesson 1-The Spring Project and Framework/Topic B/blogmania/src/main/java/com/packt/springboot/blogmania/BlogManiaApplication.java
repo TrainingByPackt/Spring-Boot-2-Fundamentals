0b9c305258d57d0392f096d8dbed55d4f6dfca9e
Patrick Cornelißen <pcornelissen@orchit.de>
2018-03-15 00:58:00 +0100

    Initial import of the generated java project for lesson 1 topic B

---
+++ Lesson 1-The Spring Project and Framework/Topic B/blogmania/src/main/java/com/packt/springboot/blogmania/BlogManiaApplication.java
@@ -0,0 +1,12 @@
+package com.packt.springboot.blogmania;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogManiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogManiaApplication.class, args);
+	}
+}
