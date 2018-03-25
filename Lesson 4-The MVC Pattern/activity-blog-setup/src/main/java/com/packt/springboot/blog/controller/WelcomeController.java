860125cacf9585130eb09bfbf32187235d81158d
Alexander Sparkowsky <info@roamingthings.de>
2018-03-25 21:29:03 +0200

    Add solution for activity

---
+++ Lesson 4-The MVC Pattern/activity-blog-setup/src/main/java/com/packt/springboot/blog/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.packt.springboot.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String renderWelcome() {
+        return "welcome";
+    }
+}
