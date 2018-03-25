b51043c20788f160d2136d8977e6a7b6718638e7
Alexander Sparkowsky <info@roamingthings.de>
2018-03-25 15:09:38 +0200

    Add Webstarter exercise

---
+++ Lesson 4-The MVC Pattern/webstarter-exercise/src/main/java/com/packt/springboot/webstarter/WebstarterExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.packt.springboot.webstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstarterExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstarterExerciseApplication.class, args);
+	}
+
+}
