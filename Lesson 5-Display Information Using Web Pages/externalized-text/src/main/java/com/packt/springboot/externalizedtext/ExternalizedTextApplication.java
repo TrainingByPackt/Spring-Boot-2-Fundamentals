63db66ec677c494a05571b90444d577af54319bb
Alexander Sparkowsky <info@roamingthings.de>
2018-04-02 20:15:10 +0200

    Add externalize string example

---
+++ Lesson 5-Display Information Using Web Pages/externalized-text/src/main/java/com/packt/springboot/externalizedtext/ExternalizedTextApplication.java
@@ -0,0 +1,12 @@
+package com.packt.springboot.externalizedtext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalizedTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalizedTextApplication.class, args);
+	}
+}
---
+++ Lesson 5-Display Information Using Web Pages/externalized-text/src/main/java/com/packt/springboot/externalizedtext/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.packt.springboot.externalizedtext.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/")
+    public String showWelcome() {
+        return "welcome";
+    }
+}
