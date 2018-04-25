7c6045ec464ec10ec8c4f3dc293e4e180f9b149b
Michael Piefel <entwurf@piefel.de>
2018-04-25 21:48:31 +0200

    Add simple security

--- blogmania/src/main/java/com/packt/springboot/blogmania/config/SecurityConfiguration.java
+++ blogmania/src/main/java/com/packt/springboot/blogmania/config/SecurityConfiguration.java
@@ -3,12 +3,20 @@ package com.packt.springboot.blogmania.config;
 import com.packt.springboot.blogmania.author.Author;
 import com.packt.springboot.blogmania.author.AuthorRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 
+@Slf4j
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final AuthorRepository authorRepository;
 
     @Bean
@@ -16,4 +24,23 @@ public class SecurityConfiguration {
         return authorRepository.findById(1L).orElseThrow(() -> new IllegalStateException("Author with id 1 not found"));
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "/webjars/**", "/login").permitAll()
+                .antMatchers("/**").hasRole("USER")
+                .and()
+                .formLogin().loginPage("/login")
+                .and()
+                .httpBasic().realmName("blogmania")
+        ;
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER"));
+    }
 }
--- blogmania/src/main/java/com/packt/springboot/blogmania/homepage/HomePageController.java
+++ blogmania/src/main/java/com/packt/springboot/blogmania/homepage/HomePageController.java
@@ -1,16 +1,19 @@
 package com.packt.springboot.blogmania.homepage;
 
 import com.packt.springboot.blogmania.blogpost.BlogPost;
-import com.packt.springboot.blogmania.blogpost.BlogPostRepository;
 import com.packt.springboot.blogmania.blogpost.BlogPostService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomePageController {
@@ -29,6 +32,16 @@ public class HomePageController {
     public void addDefaultAttributes(Model model) {
         int allPostsCount = blogPostService.numberOfBlogPosts();
         model.addAttribute("allPostsCount", allPostsCount);
+    }
+
+    @GetMapping("/login")
+    public String login(Model model,
+                        @RequestParam Optional<String> error) {
+        if (error.isPresent()) {
+            log.info("Incorrect login, warning the user");
+            model.addAttribute("loginError", "true");
+        }
+        return "login";
     }
 
 }
