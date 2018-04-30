c2bdf3ab58a92fbecd0240ca84f8224a2b5b813a
Alexander Sparkowsky <info@roamingthings.de>
2018-04-30 21:27:53 +0200

    Add basic-thymeleaf-template

---
+++ Lesson 5-Display Information Using Web Pages/basic-thymeleaf-template/src/main/java/com/packt/springboot/basicthymeleaftemplate/BasicThymeleafTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.packt.springboot.basicthymeleaftemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicThymeleafTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicThymeleafTemplateApplication.class, args);
+	}
+}
---
+++ Lesson 5-Display Information Using Web Pages/basic-thymeleaf-template/src/main/java/com/packt/springboot/basicthymeleaftemplate/welcome/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.packt.springboot.basicthymeleaftemplate.welcome;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/")
+    public String welcomePage(Model model) {
+        model.addAttribute("welcome", "This is the welcome message.");
+
+        return "welcome";
+    }
+}
