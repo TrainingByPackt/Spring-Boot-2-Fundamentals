30996cc7f15ea1e4bcec90ebee7b6003da87aa7a
Patrick Cornelißen <pcornelissen@orchit.de>
2018-06-22 07:19:49 +0200

    fix solution for exercise in topic b

--- Lesson 2-Building a Spring Application/Topic B/blogmania-exercise-solution/src/main/java/com/packt/springboot/blogmania/exercise/FakeRandom.java
+++ Lesson 2-Building a Spring Application/Topic B/blogmania-exercise-solution/src/main/java/com/packt/springboot/blogmania/exercise/FakeRandom.java
@@ -4,17 +4,12 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-
-@Component("randomizer") @Primary @Profile("test")
+@Component("randomizer")
+@Primary
+@Profile("test")
 public class FakeRandom implements Randomizer {
     @Override
     public double getRandom() {
-        try {
-            return SecureRandom.getInstanceStrong().nextDouble();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Could not initialize random number generator",e);
-        }
+        return 2.7182818284590452354;
     }
 }
--- Lesson 2-Building a Spring Application/Topic B/blogmania-exercise-solution/src/main/java/com/packt/springboot/blogmania/exercise/RealRandom.java
+++ Lesson 2-Building a Spring Application/Topic B/blogmania-exercise-solution/src/main/java/com/packt/springboot/blogmania/exercise/RealRandom.java
@@ -2,18 +2,13 @@ package com.packt.springboot.blogmania.exercise;
 
 import org.springframework.stereotype.Component;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Component("randomizer")
 //@Profile("!test") // this is another was if you don't use @Primary on the other bean
 public class RealRandom implements Randomizer {
     @Override
     public double getRandom() {
-        try {
-            return SecureRandom.getInstanceStrong().nextDouble();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("Could not initialize random number generator",e);
-        }
+        return ThreadLocalRandom.current().nextDouble();
     }
 }
