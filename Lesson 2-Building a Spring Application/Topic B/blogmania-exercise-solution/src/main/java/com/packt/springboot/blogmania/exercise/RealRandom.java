79a3f70c0c9833783e2ce6e8268a1cf667ebd2a7
Alexander Sparkowsky <info@roamingthings.de>
2018-10-14 14:09:27 +0200

    Fix typo

--- Lesson 2-Building a Spring Application/Topic B/blogmania-exercise-solution/src/main/java/com/packt/springboot/blogmania/exercise/RealRandom.java
+++ Lesson 2-Building a Spring Application/Topic B/blogmania-exercise-solution/src/main/java/com/packt/springboot/blogmania/exercise/RealRandom.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component("randomizer")
-//@Profile("!test") // this is another was if you don't use @Primary on the other bean
+//@Profile("!test") // this is another way if you don't use @Primary on the other bean
 public class RealRandom implements Randomizer {
     @Override
     public double getRandom() {
